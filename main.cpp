6d8d2a09010aee3f1d5a33fb601e3e92325de38d
Robertas Smaliukas <saturnas@gmail.com>
2012-10-04 14:36:57 +0200

    1

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int sudetis(int x, int y){
 }
 int main (int argc, char const *argv[])
 {
-	int x = 10, y = 5;
+	int x = 15, y = 25;
 	cout << "Sveika aritmetika!";
 	cout << "x + y = " << sudetis(x,y);
 	return 0;
